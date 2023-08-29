52caa9e8f31eae7a698c253d9b6ee8001c64152c
Jamal Farea <72351012+GmalFari@users.noreply.github.com>
2023-08-29 17:11:24 +0300

    Update create-account.tsx

--- components/createAccountApp/create-account.tsx
+++ components/createAccountApp/create-account.tsx
@@ -22,7 +22,7 @@ const CreateAccount:React.FC<ChildComponentProps> = ({
     <>
    <div className="rounded-lg max-w-sm mx-4 w-11/12 lg:max-w-lg">
    <div className="rounded-lg  border border-secondary-color-theme ">
-      <div className="">
+      <div className="px-4">
       <HeaderComponent mainText={mainText} secondText={secondText}  />
       <MultiStepsRectangles />
       <Component  />
