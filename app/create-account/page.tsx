0e0baa4c1e000f0d2fe1830f446312c94067d977
GmalFari <gmalfari926@gmail.com>
2023-09-02 02:49:27 +0300

    a

--- app/create-account/page.tsx
+++ app/create-account/page.tsx
@@ -3,7 +3,10 @@ import AccountPalanceNav from '@/components/AccountPalanceNav'
 const createAccountPage = () => {
   return (
     <>
+    <div className='mx-4'>
+
     <AccountPalanceNav />
+    </div>
     </>
   )
 }
