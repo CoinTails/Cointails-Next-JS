df0743f0820d86f73cf190c386a3ad992adf5981
Jamal Farea <72351012+GmalFari@users.noreply.github.com>
2023-09-02 19:32:28 +0300

    Update Wallets.tsx

--- components/loginApp/Wallets.tsx
+++ components/loginApp/Wallets.tsx
@@ -17,7 +17,7 @@ const Wallets = () => {
         </h2>
       <h3 className='my-8 font-normal text-2xl'>Select Wallet</h3>
       </div>
-    <div className='grid grid-cols-4 justify-center mb-8'>
+    <div className='grid sm:grid-cols-3  lg:grid-cols-4 justify-center mb-8'>
       <div className='flex flex-col items-center'>
         <Image className='mb-3' src={CoinBase} alt="coinBase" />
         <h3>CoinBase</h3>
