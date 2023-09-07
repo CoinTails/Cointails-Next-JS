5f8e2c555f553be69d70ef3188a279f9c43b9599
Jamal Farea <gmalfari926@gmail.com>
2023-09-07 18:27:32 +0300

    Update HorizonalScrollNav.tsx

--- components/cards/HorizonalScrollNav.tsx
+++ components/cards/HorizonalScrollNav.tsx
@@ -3,10 +3,10 @@ import { ShoppingBag,Home,BadgePercent, GalleryHorizontal} from 'lucide-react'
 const HorizonalScrollNav = () => {
   return (
     <div className='px-2 overflow-x-scroll  sm:px-4 flex font-normal items-center 
-     justify-between min-w-[768px] h-20 bg bg-opacity-first-color' 
+     justify-between h-20 bg bg-opacity-first-color' 
     style={{ scrollbarColor: 'none' }}>
         
-        <div className=' flex justify-center items-center text-first-dark-color
+        <div className=' flex mx-28 justify-center items-center text-first-dark-color
         transform transition-all duration-200 hover:scale-105 hover:font-bold hover:text-primary-first-color
       '>
         
