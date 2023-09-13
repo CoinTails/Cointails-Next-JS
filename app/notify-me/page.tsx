14ac3c094f5e7785a082f605c0e5ef9db687e0ed
GmalFari <gmalfari926@gmail.com>
2023-09-13 12:07:53 +0300

    a

--- app/notify-me/page.tsx
+++ app/notify-me/page.tsx
@@ -11,7 +11,6 @@ const Home=(
       <div className="">
       <NotifyMe/>
       </div>
-      <div className="animate__animated animate__slideInDown">Hello, World!</div>
       {/* <h1 className="animate__animated animate__bounce">An animated element</h1> */}
 
       </main>
