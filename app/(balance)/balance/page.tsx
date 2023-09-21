72226ff3d1d11c60558e0c7b4638d2107ddafd48
GmalFari <gmalfari926@gmail.com>
2023-09-21 12:55:04 +0300

    Add land-scape portion

--- app/(balance)/balance/page.tsx
+++ app/(balance)/balance/page.tsx
@@ -6,7 +6,7 @@ const Home=(
   
   return (    
     <div className=' w-full mx-auto text-center max-w-[992px]'>
-      <AccountPalanceNav title={"create Your Palance"}  
+      <AccountPalanceNav title={"create Your Balance"}  
       component={MultistepPalance}/>
 
       </div>
